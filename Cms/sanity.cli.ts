4d5150f92a599d3b4001defcd01f94c1dd29e156
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-02-25 18:01:14 +0100

    sanity install

---
+++ Cms/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pjek0hua',
+    dataset: 'production'
+  }
+})
---
+++ Frontend/src/app/studio/[[...index]]/page.tsx
@@ -0,0 +1,4 @@
+"use client"
+
+import {NextStudio} from "next-sanity/studio";
+import config from "../../n../../sanity.config";
